48544470593575671746c36dc20525086ffab8f4
avmoran92 <avmoran@wustl.edu>
2017-07-05 13:53:25 -0500

    Finished assignment

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -4,6 +4,7 @@ import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
@@ -23,5 +24,22 @@ public class SearchController {
     }
 
     // TODO #1 - Create handler to process search request and display results
+
+    @RequestMapping(value = "results")
+    public String search(Model model, @RequestParam String searchType, @RequestParam String
+            searchTerm) {
+
+        model.addAttribute("columns", ListController.columnChoices);
+
+        if (searchType.equals("all")) {
+            model.addAttribute("jobs", JobData.findByValue(searchTerm));
+        } else if (ListController.columnChoices.keySet().contains(searchType)){
+            model.addAttribute("jobs", JobData.findByColumnAndValue(searchType, searchTerm));
+        } else {
+            model.addAttribute("error", "The column you chose does not exist.");
+        }
+
+        return "search";
+    }
 
 }
